adb2a2816838902b5bcb6a04be18439c2aab3e39
Saptyadeep <bsaptyadeep@gmail.com>
2024-07-20 21:52:20 +0530

    done

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
-export const SERVER_BASE_URL = "https://data-matter-backend.onrender.com/"
+export const SERVER_BASE_URL = "https://data-matter-backend.onrender.com"
 
 export const GET_USER_GOOGLE_PROFILE_URL = "https://www.googleapis.com/oauth2/v3/tokeninfo"
