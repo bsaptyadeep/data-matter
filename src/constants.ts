b5f5ec7f7986911d56b845445cd7c6b8f57810d9
Saptyadeep <bsaptyadeep@gmail.com>
2024-08-12 00:33:40 +0530

    constants.ts base url updated

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
-// export const SERVER_BASE_URL = "https://data-matter-backend.onrender.com"
+export const SERVER_BASE_URL = "https://data-matter-backend.onrender.com"
 
-export const SERVER_BASE_URL = "http://localhost:8000"
+// export const SERVER_BASE_URL = "http://localhost:8000"
 
 export const GET_USER_GOOGLE_PROFILE_URL = "https://www.googleapis.com/oauth2/v3/tokeninfo"
